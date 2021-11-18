ce9549547e56028101bb07b468b0c48cf22d229b
Deborah Dike <deborah.r.dike@gmail.com>
2021-11-18 17:10:15 +0000

    Simplify code

--- run.py
+++ run.py
@@ -42,35 +42,48 @@ def display_main_menu():
     print('-- PLAY [1]')
     print('-- GAME OPTIONS [2]')
 
-    while True:
-        userInput = input('Select menu option: ')
+    await_input('Select menu option: ', process_main_menu_selection)
 
-        if userInput == '2':
-            display_game_options_menu()
+def process_main_menu_selection(input):
+    """
+    Displays game menu options if user input is as expected value
+    """
+    if input == '2':
+        display_game_options_menu()
+        await_input('Select game option to toggle: ', toggle_game_option, display_game_options_menu)
 
 def display_game_options_menu():
     """
     Prints game manu options to console
     """
-    global input_mode
-    global difficulty
-    global enable_hints
-
     game_options_str = '---- Input mode [1]: {}\n---- Difficulty [2]: {}\n---- Enable hints [3]: {}'
     print(game_options_str.format(InputMode.get_description(input_mode), Difficulty.get_description(difficulty), enable_hints))
 
+def toggle_game_option(input):
+    """
+    Toggles game option based on user input and current settings
+    """
+    global input_mode
+    global difficulty
+    global enable_hints
+    
+    if input == '1':
+        input_mode = input_mode + 1 if input_mode < 3 else 1
+    elif input == '2':
+        difficulty = difficulty + 1 if difficulty < 4 else 1
+    elif input == '3':
+        enable_hints = not enable_hints
+
+def await_input(prompt, execute, update_terminal = None):
+    """
+    Awaits input, gives a prompt, executes a function based on input
+    and calls a function to update the terminal if provided
+    """
     while True:
-        userInput = input('Select game option to toggle: ')
-
-        if userInput == '1':
-            input_mode = input_mode + 1 if input_mode < 3 else 1
-        elif userInput == '2':
-            difficulty = difficulty + 1 if difficulty < 4 else 1
-        elif userInput == '3':
-            enable_hints = not enable_hints
-
-        game_options_str = '---- Input mode [1]: {}\n---- Difficulty [2]: {}\n---- Enable hints [3]: {}'
-        print(game_options_str.format(InputMode.get_description(input_mode), Difficulty.get_description(difficulty), enable_hints))
+        userInput = input(prompt)
+        execute(userInput)
+        if (update_terminal != None):
+            update_terminal()
 
 def main():
     """
