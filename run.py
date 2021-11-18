9cfe3ad0fb03648f0b887e00b6510cb1fd91113b
Deborah Dike <deborah.r.dike@gmail.com>
2021-11-18 17:42:18 +0000

    Add ability to navigate back to the main menu

--- run.py
+++ run.py
@@ -41,6 +41,7 @@ def display_main_menu():
     """
     print('-- PLAY [1]')
     print('-- GAME OPTIONS [2]')
+    print('-- QUIT [3]')
 
     await_input('Select menu option: ', process_main_menu_selection)
 
@@ -50,29 +51,31 @@ def process_main_menu_selection(input):
     """
     if input == '2':
         display_game_options_menu()
-        await_input('Select game option to toggle: ', toggle_game_option, display_game_options_menu)
+        await_input('Select game option: ', process_game_option, display_game_options_menu)
 
 def display_game_options_menu():
     """
     Prints game manu options to console
     """
-    game_options_str = '---- Input mode [1]: {}\n---- Difficulty [2]: {}\n---- Enable hints [3]: {}'
+    game_options_str = '---- Input mode [1]: {}\n---- Difficulty [2]: {}\n---- Enable hints [3]: {}\n-- Return to main menu [4]'
     print(game_options_str.format(InputMode.get_description(input_mode), Difficulty.get_description(difficulty), enable_hints))
 
-def toggle_game_option(input):
+def process_game_option(input):
     """
     Toggles game option based on user input and current settings
     """
     global input_mode
     global difficulty
     global enable_hints
-    
+
     if input == '1':
         input_mode = input_mode + 1 if input_mode < 3 else 1
     elif input == '2':
         difficulty = difficulty + 1 if difficulty < 4 else 1
     elif input == '3':
         enable_hints = not enable_hints
+    elif input == '4':
+        return True
 
 def await_input(prompt, execute, update_terminal = None):
     """
@@ -81,9 +84,11 @@ def await_input(prompt, execute, update_terminal = None):
     """
     while True:
         userInput = input(prompt)
-        execute(userInput)
-        if (update_terminal != None):
+        stopLoop = execute(userInput)
+        if (not stopLoop and update_terminal != None):
             update_terminal()
+        elif stopLoop == True:
+            break
 
 def main():
     """
