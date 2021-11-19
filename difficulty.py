fbe066dae8c4669d2111b1208e1ba6c95db373c7
Deborah Dike <deborah.r.dike@gmail.com>
2021-11-19 13:58:52 +0000

    Make all logical paths return (Easy mode is default)

--- difficulty.py
+++ difficulty.py
@@ -12,11 +12,10 @@ class Difficulty(Enum):
         """
         Returns the description for a given input mode
         """
-        if difficulty == 0:
-            return "5 questions in 'popular' languages"
         if difficulty == 1:
             return "5 questions in less 'popular' languages"
         if difficulty == 2:
             return "10 questions in lesser known languages"
         if difficulty == 3:
             return '26 questions in all available languages + 5 seconds to answer'
+        return "5 questions in 'popular' languages"
