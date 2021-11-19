05653c2e1f4530bba4b96fef82e48aa8726590ca
Deborah Dike <deborah.r.dike@gmail.com>
2021-11-19 14:39:57 +0000

    Use double quotes for consistency and conform to line length limit

--- difficulty.py
+++ difficulty.py
@@ -41,5 +41,8 @@ class Difficulty(Enum):
         if difficulty == 2:
             return "10 questions in lesser known languages"
         if difficulty == 3:
-            return '26 questions in all available languages + 5 seconds to answer'
+            return (
+                "26 questions in all available languages"
+                " + 5 seconds to answer"
+            )
         return "5 questions in 'popular' languages"
