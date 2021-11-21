f5c4ed1c67446ded7418275461299c30e9142bbb
Deborah Dike <deborah.r.dike@gmail.com>
2021-11-21 16:56:06 +0000

    Implement dictionary search

--- classes/gamedictionary.py
+++ classes/gamedictionary.py
@@ -1,5 +1,7 @@
 """Class for word lookup based on parts of speech."""
+import random
 from typing import Dict, List
+from classes.enums.partofspeech import PartOfSpeech
 
 
 class GameDictionary():
@@ -10,6 +12,12 @@ class GameDictionary():
     WORDS: Dict[str, List[str]]
         A dictionary of words by their identifying part of speech
         and part of speech type/specificity.
+
+    Methods
+    -------
+    search_for_word_by_type(
+            part_of_speech: PartOfSpeech, specificity: str) -> str
+        Finds and returns a word that meets the given criteria.
     """
 
     WORDS: Dict[str, Dict[str, List[str]]] = {
@@ -219,3 +227,57 @@ class GameDictionary():
             ],
         }
     }
+
+    @classmethod
+    def search_for_word_by_type(cls, part_of_speech: PartOfSpeech) -> str:
+        """Finds a random word in the WORDS dict that falls under a given part
+        of speech.
+
+        Parameters
+        ----------
+        part_of_speech
+            The part of speech that the word falls under.
+
+        Returns
+        ----------
+        str
+            A word that matches the given criteria.
+        """
+        key, specificity = cls._get_search_criteria(part_of_speech)
+
+        potential_words_by_spec = cls.WORDS.get(key)
+
+        if not specificity:
+            specificity = random.choice(list(potential_words_by_spec.keys()))
+
+        return random.choice(potential_words_by_spec.get(specificity))
+
+    @staticmethod
+    def _get_search_criteria(part_of_speech: PartOfSpeech) -> tuple:
+        """Retrieves search criteria for a given part of speech.
+
+        Parameters
+        ----------
+        part_of_speech
+            The enum representation for a part of speech.
+
+        Returns
+        ----------
+        tuple
+            Key and specificity that make up the search criteria.
+        """
+        # In order to match with a key in our dictionary, we must split the
+        # PartOfSpeech enum's name value.
+        # For values that contain a '_', this will give us a specificity with
+        # which to further narrow the search.
+        criteria = part_of_speech.value.split("_")
+
+        # We need to reverse it to get the correct order
+        criteria.reverse()
+
+        # Unpacks criteria into corresponding variables and handles case for
+        # parts of speech that have no specificity in their key
+        key, specificity = criteria if len(criteria) == 2 else (
+                criteria[0], None)
+
+        return (key + "s", specificity)
