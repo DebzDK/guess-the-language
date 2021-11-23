b9991e5540989a4f54d37ebd0402aa9aa4b83340
Deborah Dike <deborah.r.dike@gmail.com>
2021-11-23 18:25:16 +0000

    Add handling for API-specific errors

--- classes/helpers/translationhelper.py
+++ classes/helpers/translationhelper.py
@@ -1,6 +1,7 @@
 """Class to help with translations."""
 from os import environ as env
 from typing import TypeVar
+from datetime import date
 import random
 import json
 import requests
@@ -32,6 +33,19 @@ class TranslationHelper():
                         f"Error: {error}"),
                         target_language)
 
+        def get_api_error_message(code: int, detailed_error: str) -> str:
+            if code == 403:
+                return (f"FORBIDDEN ({code}) - Please ensure the DeepL "
+                        "API key has been provided.")
+            if code == 456:
+                today = date.today()
+                split_date = today.strftime("%Y %m").split()
+                limit_refresh_date = date(int(today[0]), int(today[1]) + 1, 16)
+                return (f"LIMIT REACHED ({code}) - The monthly quota has been"
+                        "reached.\nIt will reset on "
+                        f"{limit_refresh_date.strftime('%B %d, %Y')}.")
+            return detailed_error
+
         api_endpoint = "https://api-free.deepl.com/v2/translate"
         api_key = env.get("DEEPL_API_KEY", "NO_API_KEY_PROVIDED")
         language_choices = list(Language)
@@ -54,6 +68,8 @@ class TranslationHelper():
         except json.decoder.JSONDecodeError as json_error:
             if api_key == "NO_API_KEY_PROVIDED":
                 return create_translation_error(api_key, target_language)
-            return create_translation_error(json_error, target_language)
+            return create_translation_error(
+                get_api_error_message(
+                    response.status_code, json_error), target_language)
         except requests.RequestException as request_error:
             return create_translation_error(request_error, target_language)
